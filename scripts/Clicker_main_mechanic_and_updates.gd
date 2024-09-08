b5a65d82feb3805a2b515424724d17e552f4ef1d
Jose Goyo <josecegarra180@gmail.com>
2024-09-07 20:40:13 -0400

    New changes: script update and little new features

--- scripts/Clicker_main_mechanic_and_updates.gd
+++ scripts/Clicker_main_mechanic_and_updates.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var cookie_count = 0
 var cookies_por_segundo = 0
+var label_update1 = 0
 
 var mejoras = {
 	"mejora1": {
@@ -13,13 +14,18 @@ var mejoras = {
 
 @onready var label_cookies = $Label
 @onready var boton_cookie = $Button
+@onready var label_2: Label = $Button2/Label2
+
 
 func _ready():
 	label_cookies.text = str(cookie_count)
+	label_2.text = 'mejoras: ' + str(label_update1)
+
 
 func _process(delta):
 	cookie_count += cookies_por_segundo * delta
 	label_cookies.text = str(cookie_count)
+	label_2.text = 'mejoras: ' + str(label_update1)
 
 func _on_button_pressed() -> void:
 	cookie_count += 1
@@ -32,6 +38,8 @@ func comprar_mejora(nombre_mejora):
 		mejora.comprada += 1
 		cookies_por_segundo += mejora.bonificacion * mejora.comprada
 		mejora.precio *= 2
+		label_2.text = 'mejoras: ' + str(label_update1)
+		label_update1 += 1
 		print("Mejora comprada!")
 	else:
 		print("No tienes suficientes galletas.")
